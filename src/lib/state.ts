d594c4f12a88ed52605099e4b9f6821f754f27ea
fandi-ahmad <fandi4160@gmail.com>
2024-05-11 21:05:36 +0800

    add data in global state

--- src/lib/state.ts
+++ src/lib/state.ts
@@ -1,11 +1,17 @@
 import { createGlobalState } from 'react-hooks-global-state'
 
 interface typeState {
-  isLoadingScreen: boolean
+  isLoadingScreen: boolean,
+  profilePicture: string,
+  currentUsername: string,
+  idUser: number,
 };
 
 const initialState: typeState = {
-  isLoadingScreen: true
+  isLoadingScreen: true,
+  profilePicture: '',
+  currentUsername: '',
+  idUser: NaN
 };
 
 const { useGlobalState } = createGlobalState(initialState);
